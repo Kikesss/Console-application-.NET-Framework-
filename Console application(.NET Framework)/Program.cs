309c2c54cfefa3dc2ad717798b6760c38e9bf0b4
Maksim <drgivi@mail.ru>
2023-03-13 21:31:58 +0300

    test
    test

--- Console application(.NET Framework)/Program.cs
+++ Console application(.NET Framework)/Program.cs
@@ -10,143 +10,290 @@ namespace Console_application_.NET_Framework_
 {
     internal class Program
     {
-        static void first_floor()
+        static void Main(string[] args)
         {
+            Dictionary<int, int[]> floors = new Dictionary<int, int[]>();
+            floors.Add(1, new int[] { 4, 6 });
+            floors.Add(2, new int[] { 8, 10 });
+            floors.Add(3, new int[] { 12, 14 });
+            floors.Add(4, new int[] { 16, 18 });
+            floors.Add(5, new int[] { 20, 22 });
+
+            int floor = 1;
+            Console.WriteLine($"Ты зашел в пятиэтажное здание");
+            Do(floor, floors);
+            string move;
+
+            int money = 500;
+
+
+
             for (int i = 0; i < 100; i++)
             {
-                Console.WriteLine("Ты на первом этаже, на нём пять комнат (12) (13) (14) (15) (16)");
-                Console.Write("Куда пойдешь(Вверх, Вниз, Выйти): ");
-                string step = Console.ReadLine();
-                if (step == "Вверх" || step == "вверх")
+                move = Console.ReadLine();
+                if (move == "Вверх" || move == "вверх")
                 {
-                    second_floor();
-                    break;
+                    if (floor != 5)
+                    {
+                        floor = Up(floor);
+                    }
+                    else
+                    {
+                        Console.WriteLine("А выше некуда");
+                    }
+                    Do(floor, floors);
                 }
 
-                if (step == "Вниз" || step == "вниз")
+                else if (move == "Вниз" || move == "вниз")
                 {
-                    Console.WriteLine("А ниже некуда");
+                    if (floor != 1)
+                    {
+                        floor = Down(floor);
+                    }
+                    else
+                    {
+                        Console.WriteLine("А ниже некуда");
+                    }
+                    Do(floor, floors);
                 }
 
-                if (step == "Выйти" || step == "выйти")
+                else if (move == "Выйти" || move == "выйти")
                 {
-                    Console.WriteLine("До встречи :3 ");
-                    break;
+                    if (floor == 1)
+                    {
+                        Console.WriteLine("До встречи :) ");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("С окна спрыгнуть?");
+                    }
+                    Do(floor, floors);
+                }
+
+                else if (move == "казино")
+                {
+                    Console.WriteLine("Добро пожаловать в казино LO$E MONEY");
+                    for (; ; )
+                    {
+                        Casino(money);
+                        Console.Write("Пойти на выход?(Да/Нет): ");
+                        string choiceYesOrNot = Console.ReadLine();
+                        if (choiceYesOrNot == "да")
+                        {
+                            break;
+                            
+                        }
+
+                        
+                    }
+
                 }
 
                 else
                 {
-                    Console.WriteLine("Я не знаю такой команды :( ");
+                    Console.WriteLine("Я не знаю такой команды");
+                    Do(floor, floors);
                 }
             }
+
+
         }
 
-        static void second_floor()
+        static int Up(int floor)
+        {
+            floor++;
+            return floor;
+        }
+
+        static int Down(int floor)
+        {
+            floor--;
+            return floor;
+        }
+        static void Do(int floor, Dictionary<int, int[]> floors)
+        {
+            Console.WriteLine("-------------------------------------------------");
+            Console.Write($"Ты на {floor} этаже с комнатами ");
+            for (int i = 0; i < floors[floor].Length; i++)
+            {
+                Console.Write($"{floors[floor][i]} ");
+            }
+            Console.WriteLine();
+            if (floor == 1)
+            {
+                Console.WriteLine("(Выйти/Вверх/Казино)");
+            }
+            else if (floor == 5)
+            {
+                Console.WriteLine("(Выйти/Вниз)");
+            }
+            else
+            {
+                Console.WriteLine("(Выйти/Вверх/Вниз)");
+            }
+
+            Console.Write("Что делать будешь: ");
+
+        }
+
+        static int Casino(int money)
         {
 
-            Console.WriteLine("Ты на втором этаже, на нём четыре комнаты (17) (18) (19) (20)");
-            Console.Write("Куда пойдешь(Вверх, Вниз): ");
-            string step = Console.ReadLine();
-            if (step == "Вверх" || step == "вверх")
-            {
-                third_floor();
-            }
-
-            if (step == "Вниз" || step == "вниз")
-            {
-                first_floor();
-            }
             
-            else
+            Console.WriteLine($"Баланс: {money}$");
+            Console.Write("(Выйти/Ставка): ");
+            string choice = Console.ReadLine();
+            if (choice == "Ставка" || choice == "ставка")
             {
-                Console.WriteLine("Я не знаю такой команды :( ");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(0 + " ");
+                for (int i = 1; i < 37; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write(i + " ");
+                    }
+
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(i + " ");
+                    }
+
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.Write("Введи размер ставки($): ");
+                int bet = Convert.ToInt32(Console.ReadLine());
+                if (bet <= money)
+                {
+                    money = money - bet;
+                    Console.Write("(Красное/Чёрное/Чётное/Нечётное/Диапазон(1-18;19-36)/Число): ");
+                    string play = Console.ReadLine();
+
+                    Random random = new Random();
+                    int number = random.Next(0, 36);
+
+                    if (play == "красное" || play == "нечетное")
+                    {
+                        if (number % 2 != 0)
+                        {
+                            money += (bet * 2) + bet;
+                        }
+
+                        else
+                        {
+                            bet = 0;
+                        }
+                    }
+                    else if (play == "черное" || play == "четное")
+                    {
+                        if (number % 2 == 0)
+                        {
+                            money += (bet * 2) + bet;
+                        }
+
+                        else
+                        {
+                            bet = 0;
+                        }
+                    }
+                    else if (play == "диапазон")
+                    {
+                        Console.Write("(первый/второй): ");
+                        string choiceDiapason = Console.ReadLine();
+                        if (choiceDiapason == "первый")
+                        {
+                            if (number <= 18)
+                            {
+                                money += (bet * 2) + bet;
+                            }
+                            else
+                            {
+                                bet = 0;
+                            }
+
+                        }
+
+                        else if (choiceDiapason == "второй")
+                        {
+                            if (number > 18)
+                            {
+                                money += (bet * 2) + bet;
+                            }
+                            else
+                            {
+                                bet = 0;
+                            }
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Нет такого диапазона");
+                        }
+                    }
+                    else if (play == "число")
+                    {
+                        Console.Write("Введи число: ");
+                        int choiceNumber = Convert.ToInt32(Console.ReadLine());
+                        if (choiceNumber == number)
+                        {
+                            money += (bet * 36) + bet;
+                        }
+                    }
+
+                    if (number == 0)
+                    {
+                        Console.Write("Выпавшее число: ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(number);
+                    }
+                    else if (number % 2 == 0)
+                    {
+                        Console.Write("Выпавшее число: ");
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine(number);
+                    }
+                    else if (number % 2 != 0)
+                    {
+                        Console.Write("Выпавшее число: ");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(number);
+                    }
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine($"Баланс: {money}$");
+
+                    
+                    
+                }
+
+                else
+                {
+                    Console.WriteLine("У тебя нет таких денег :(");
+                }
             }
+
+            return money;
+
+
+
+
+
         }
-
-        static void third_floor()
-        {
-
-            Console.WriteLine("Ты на третьем этаже, на нём три комнаты (21) (22) (23)");
-            Console.Write("Куда пойдешь(Вверх, Вниз): ");
-            string step = Console.ReadLine();
-            if (step == "Вверх" || step == "вверх")
-            {
-                fourth_floor();
-            }
-
-            if (step == "Вниз" || step == "вниз")
-            {
-                second_floor();
-            }
-            else
-            {
-                Console.WriteLine("Я не знаю такой команды :( ");
-            }
-        }
-
-        static void fourth_floor()
-        {
-            Console.WriteLine("Ты на четвертом этаже, на нём две комнаты (24) (25)");
-            Console.Write("Куда пойдешь(Вверх, Вниз): ");
-            string step = Console.ReadLine();
-            if (step == "Вверх" || step == "вверх")
-            {
-                fifth_floor();
-            }
-
-            if (step == "Вниз" || step == "вниз")
-            {
-                third_floor();
-            }
-            else
-            {
-                Console.WriteLine("Я не знаю такой команды :( ");
-            }
-        }
-
-        static void fifth_floor()
-        {
-
-            Console.WriteLine("Ты на пятом этаже, на нём одна комната (26)");
-            Console.Write("Куда пойдешь(Вверх, Вниз): ");
-            string step = Console.ReadLine();
-
-            while (step == "Вверх" || step == "вверх")
-            {
-                Console.WriteLine("А выше некуда");
-                Console.WriteLine("Ты на пятом этаже, на нём одна комната (26)");
-                Console.Write("Куда пойдешь(Вверх, Вниз): ");
-                step = Console.ReadLine();
-
-            }
-
-            if (step == "Вниз" || step == "вниз")
-            {
-               fourth_floor();
-            }
-            else
-            {
-               Console.WriteLine("Я не знаю такой команды :( ");
-            }
-            
-        }
-
-        
-        
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Ты зашел в пятиэтажное здание");
-            first_floor();
-        }
-
-        
-
-        
-
-        
-
-        
-
-        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
